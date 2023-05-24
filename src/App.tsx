371a72f99170b1411d0a1095286950830146da6c
huyuneko0212 <wintercat012345@gmail.com>
2023-05-24 23:22:36 +0900

    ひとまず完成

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import React,{useCallback,useState,useEffect} from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin,{DateClickArg} from "@fullcalendar/interaction";
+import './App.css';
+import { start } from 'repl';
+import { log } from 'console';
+type Event = {
+  start: string;
+  // end: string;
+  display: string;
+  color: string;
+}
+function App() {
+    const handleDateClick = useCallback((arg: DateClickArg) => {
+    alert(arg.date);
+  }, []);
+  const [events,setEvents] = useState<Event[]>([]);
+  
+  
+  
+  return (
+    <>
+    <FullCalendar
+    plugins={[dayGridPlugin,interactionPlugin]}
+    initialView='dayGridMonth'
+    locale="ja"
+    editable={false}
+    events={events}
+    selectable={true}
+    select={function(arg) {
+      let list =[...events,{
+        start: arg.startStr,
+        // end: arg.endStr,
+        display: 'background',
+        color: 'red'
+      }]
+      list.sort((a,b) => {
+        if(a.start < b.start) return -1;
+        if(a.start > b.start) return 1;
+        return 0;
+      })
+      setEvents(list)
+      
+      
+    }
+  }
+  
+  />
+  {events.map((event,index) => {
+    const date = new Date(event.start)
+    console.log(date.getDate());
+    
+    return (
+      <span key={index}>
+        {date.getDate()},
+      </span>
+    )
+  })
+  }
+    </>
+
+    
+  );
+}
+
+export default App;
