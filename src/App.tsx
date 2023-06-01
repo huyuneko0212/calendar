a7d7e82de5d883b350e4bc5bae737ff2eea00a66
huyuneko0212 <wintercat012345@gmail.com>
2023-06-01 21:15:09 +0900

    重複排除

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,76 @@
-import React,{useCallback,useState,useEffect} from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin,{DateClickArg} from "@fullcalendar/interaction";
-import './App.css';
-import { start } from 'repl';
-import { log } from 'console';
+import React, { useCallback, useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import "./App.css";
+import { start } from "repl";
+import { log } from "console";
 type Event = {
   start: string;
   // end: string;
   display: string;
   color: string;
-}
+};
 function App() {
-    const handleDateClick = useCallback((arg: DateClickArg) => {
+  const handleDateClick = useCallback((arg: DateClickArg) => {
     alert(arg.date);
   }, []);
-  const [events,setEvents] = useState<Event[]>([]);
-  const one_way=330;
-  let attendance_days=events.length;
-  const commute_expenses=attendance_days*one_way*2;
-  
+  const [events, setEvents] = useState<Event[]>([]);
+  const one_way = 330;
+  let attendance_days = events.length;
+  const commute_expenses = attendance_days * one_way * 2;
+
   return (
     <>
-    <FullCalendar
-    plugins={[dayGridPlugin,interactionPlugin]}
-    initialView='dayGridMonth'
-    locale="ja"
-    editable={false}
-    events={events}
-    selectable={true}
-    select={function(arg) {
-      let list =[...events,{
-        start: arg.startStr,
-        // end: arg.endStr,
-        display: 'background',
-        color: 'red'
-      }]
-      list.sort((a,b) => {
-        if(a.start < b.start) return -1;
-        if(a.start > b.start) return 1;
-        return 0;
-      })
-      setEvents(list)
-      
-      
-    }
-  }
-  
-  />
-  <span>
-  出勤日：
-  </span>
-  {events.map((event,index) => {
-    const date = new Date(event.start)
-    console.log(date.getDate());
-    
-    return (
-      <span key={index}>
-        {date.getDate()},
-      </span>
-    )
-  })
-}
-  <hr />
-  <span>通勤費：</span>
-  {commute_expenses}
-    </>
+      <div style={{display:"flex", marginTop:90}}>
+        <div style={{ width: "60%", height: "50%" }}>
+          <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            locale="ja"
+            editable={false}
+            // events={events}
+            selectable={true}
+            select={function (arg) {
+              
+              let list = [
+                ...events,
+                {
+                  start: arg.startStr,
+                  // end: arg.endStr,
+                  display: "background",
+                  color: "blue",
+                },
+              ];
+              const uniqueList=Array.from(new Map(list.map((item)=>[item.start,item])).values());
+              console.log(uniqueList);
+              
+              uniqueList.sort((a, b) => {
+                if (a.start < b.start) return -1;
+                if (a.start > b.start) return 1;
+                return 0;
+              });
+              setEvents(uniqueList);
+            }}
+          />
+        </div>
+        <div style={{marginTop:70,marginLeft:50 ,width:"50%"}}>
+          <span>出勤日：</span>
+          {events.map((event, index) => {
+            const date = new Date(event.start);
+            console.log(date.getDate());
 
-    
+            return <span key={index}>{date.getDate()},</span>;
+          })}
+          <hr />
+          <span>出社回数：：</span>
+          {events.length}
+          <hr />
+          <span>通勤費：</span>
+          {commute_expenses}
+        </div>
+      </div>
+    </>
   );
 }
 
